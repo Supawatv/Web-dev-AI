72a9689ed3d8d7dac5e9ba3e57c645752245bad4
Supawatv <supawatv@gmail.com>
2019-12-12 06:01:14 -0500

    Final Submission without Writing

--- draw.js
+++ draw.js
@@ -1,40 +1,66 @@
-//setup = intro: Click and drag to add an item in your container
-// error1 = You may only add 4 items to your container
+// Supawat Vitoorapakorn
+// Main P5js code for draw
 
 children = [];
 create_X = 0;
 create_Y = 0;
-max_child = 4;
+max_child = 4; //determines max rectanges drawn
 
+// Check if mobile device
+function is_mobile() {
+	if (windowWidth < 700) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Setup canvas
 function setup() {
 	children = [];
-	var myCanvas = createCanvas(450, 450);
-	myCanvas.parent('draw');
+
+	// Responsive design for mobile
+	if (is_mobile() == true) {
+		display_ratio = .85;
+		update_UI("modal2", "Tap here to reset.")
+	} else {
+		display_ratio = .31;
+	}
+	var myCanvas = createCanvas(windowWidth * display_ratio, windowWidth *
+		display_ratio);
+	myCanvas.parent('draw'); //attach canvas
 	draw_div();
 	print("setup")
 }
 
+// Ensure Responsiveness to screen size change
+function windowResized() {
+	setup();
+	update_prediction();
+}
+
+// Update MVC view
 function draw() {
 	render();
 
 }
 
+// Controller 
 function mousePressed() {
+	// Records where mouse is pressed
 	create_X = mouseX;
 	create_Y = mouseY;
-
-
 }
 
+// Controller rectangular item
 function mouseDragged() {
 	if (children.length < max_child) {
 		draw_div();
 		rect(create_X, create_Y, mouseX - create_X, mouseY - create_Y)
 	} else {
-		print("error1")
-		str =
+		Str =
 			'Sorry. No more than 4 rectanges for now. Please reset ("R").'
-		update_modal(str);
+		update_modal(Str);
 		update_focus("on");
 		return
 	}
@@ -46,15 +72,17 @@ async function mouseReleased() {
 		create_child(create_X, create_Y, mouseX - create_X, mouseY -
 			create_Y)
 		render();
+		update_suggest_remove();
 		await update_prediction();
+		update_suggest_add();
 	} else {
 		update_focus("off");
 	}
 
 }
 
-
-
+// inputs coordinates
+// outputs objects in global array
 function create_child(x, y, w, h) {
 	var child = {
 		x: x,
@@ -62,12 +90,10 @@ function create_child(x, y, w, h) {
 		w: w,
 		h: h
 	}
-
 	children.push(child);
 }
 
-
-
+// View rectangle from stored array
 function render() {
 	draw_lightgray();
 	for (i in children) {
@@ -75,7 +101,7 @@ function render() {
 	}
 }
 
-
+// Helper function to change p5js draw
 function draw_lightgray() {
 	noStroke();
 	c = color("lightgray");
@@ -83,7 +109,7 @@ function draw_lightgray() {
 	noStroke();
 }
 
-
+// Draw the background
 function draw_div() {
 	rectMode(RADIUS);
 	background("#d3d3d3");
@@ -103,10 +129,11 @@ function draw_div() {
 	rectMode(CORNER);
 }
 
-
+// Control
 function keyTyped() {
 	if (key === 'r') {
 		setup();
+		update_suggest_remove();
 		update_prediction();
 	} else if (key === 's') {
 		save();
--- main.js
+++ main.js
@@ -1,49 +1,51 @@
+// Supawat Vitoorapakorn
+// Teachable Machine Back End + GUI
+
 cur_predict = [];
 update_focus("off");
 
-// Teachable Machine Backend
+
+// Teachable Machine Backend --- 
 const canvas = document.getElementById("defaultCanvas0");
 
 setTimeout(function() {
 	setConstant();
 }, 500);
 
+// Delay teachleable machine from connecting
+// with p5js to prevent error
 function setConstant() {
 	init();
 }
 
-
-
+// Initialize teachable machine backend
 async function init() {
 	const video = document.getElementById("debugger")
 	console.log("running init...")
-	const modelURL = "model.json";
+	const ModelURL = "model.json";
 	const metadataURL = "metadata.json";
-
 	update_modal("Loading AI please wait...")
-	model = await tmImage.load(modelURL, metadataURL);
-	maxPredictions = model.getTotalClasses();
+
+	// Wait for Model to load
+	Model = await tmImage.load(ModelURL, metadataURL);
+	maxPredictions = Model.getTotalClasses();
+
+	// Make a prediction and update modal
 	check_ai();
 	update_modal(
 		"AI is loaded. Click and drag to draw in the: ");
+	// Turn off splash loading screen
 	update_overlay("off");
 }
 
 
 
-async function loop() {
-	console.log("loop entered...");
-	await predict();
-	window.requestAnimationFrame(loop);
-}
-
-
 // Check Teachable machine for prediction; return prediction
 async function check_ai() {
-	results = await model.predict(document.getElementById(
+	// Predict visually from HTML element
+	results = await Model.predict(document.getElementById(
 		"defaultCanvas0"))
-
-	// Sort Results
+	// Sort Results by probability
 	byProb = results;
 	byProb.sort(function(a, b) {
 		return b.probability.toFixed(2) - a.probability.toFixed(2);
@@ -52,29 +54,29 @@ async function check_ai() {
 	return byProb;
 }
 
-// FrontEnd
+// FrontEnd ---
 
 // Main Update after mouse released
 async function update_prediction() {
 	modal_class = document.getElementById("predict_class");
 	await check_ai();
 	result = cur_predict;
-
 	top_result = result[0].className;
 	// Update UI
-	modal_class.innerHTML = top_result;
+	modal_class.innerHTML = "I see " + top_result;
 	update_code(top_result);
 	update_mini(top_result);
 }
 
-
-function update_modal(str) {
+// General function to change Model
+function update_modal(Str) {
 	modal_class = document.getElementById("predict_class");
-	modal_class.innerHTML = str;
+	modal_class.innerHTML = Str;
 
 }
 
-
+// Updates corresponding HTML and CSS
+// based on the top prediction
 function update_code(top_predict) {
 	// Select element
 	HTML_code = "codes/" + top_predict + "_html.txt";
@@ -88,6 +90,7 @@ function update_code(top_predict) {
 }
 
 
+// Initial splash screen to load AI
 function update_overlay(state) {
 	if (state == "on") {
 		$(document).ready(function() {
@@ -100,6 +103,7 @@ function update_overlay(state) {
 	}
 }
 
+// Black overlay when user exceeds 4 rectangle
 function update_focus(state) {
 	if (state == "on") {
 		$(document).ready(function() {
@@ -112,11 +116,69 @@ function update_focus(state) {
 	}
 }
 
-
+// Update prediction icon on the bottom modal
 function update_mini(top_predict) {
-	str = "icons/" + top_predict + ".png"
+	Str = "icons/" + top_predict + ".png"
 	$(document).ready(function() {
-		$("#mini").attr("src", str);
+		$("#mini").attr("src", Str);
 	});
-
 }
+
+// Add mini map onto top bar
+function update_suggest_add() {
+	// Don't show anything if empty
+	if (cur_predict[0].className == "empty") {
+		return
+	}
+
+	// If mobile shorten list to 4 predictions
+	if (is_mobile() == true) {
+		cur_predict.length = 5;
+	}
+
+	for (i in cur_predict) {
+		className = cur_predict[i].className;
+
+		// Skip if prediction is empty
+		if (className == "empty") {
+			continue;
+		}
+		img_path = "icons/" + className + ".png";
+		index = "s" + i;
+		elem = '<img class="suggest" id="_ID_" src="_SRC_" />';
+		elem = elem.replace("_ID_", index);
+		elem = elem.replace("_SRC_", img_path);
+		$(document).each(function(i) {
+			$("#bar").append(elem);
+		});
+	}
+	// Delete first suggest as to not repeat
+	$(document).ready(function() {
+		$("#s0").remove();
+	});
+}
+
+
+function update_suggest_remove() {
+	$(document).ready(function() {
+		$(".suggest").remove();
+	});
+}
+
+// Cleans up top bar
+function update_suggest() {
+	update_suggest_add();
+	update_suggest_remove();
+}
+
+// General funciton for updating UI
+function update_UI(id, Str) {
+	document.getElementById(id).innerHTML = Str;
+}
+
+// Bind even listener to reset text
+$("#modal2").on("click", function() {
+	setup();
+	update_suggest_remove();
+	update_prediction();
+});
